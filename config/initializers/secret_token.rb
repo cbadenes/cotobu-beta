5ef739e8733c66e83d52813b0da141ee4ca38941
Cabadol <cbadenes@miniluminare.(none)>
2012-04-17 20:32:49 +0200

    first commit for cotobu-beta

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CotobuBeta::Application.config.secret_token = '3453dbe46af194d8261973646e693240869e24c59744468eba4507ea8101dbc7d60e53bf55dac0dc06118c85387285c6c7e793498d181a723c9a77b0735c50c1'
